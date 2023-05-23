03c25838f65128f3fb78274204474df921166d6c
ja-calderon <105570149+ja-calderon@users.noreply.github.com>
2023-05-22 22:00:39 -0300

    se agrega info a series

--- js/portadaSeries.js
+++ js/portadaSeries.js
@@ -1,8 +1,8 @@
 
-const contenedorPeliculas = document.getElementById("contenedorPeliculas");
+const contenedorSeries = document.getElementById("contenedorSeries");
 
-if (contenedorPeliculas) {
-  fetch("datosPelicula.json")
+if (contenedorSeries) {
+  fetch("datosSeries.json")
     .then((response) => response.json())
     .then((data) => {
       data.forEach((elemento) => {
@@ -15,27 +15,28 @@ if (contenedorPeliculas) {
               "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTUxYzUwOWRlZTBmMjhiYmNkZWI2NjU0NWQ3YWNlMCIsInN1YiI6IjY0Njk3MDhhMzNhMzc2MDE3NWQyZmFmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5KqAXH6kuCBfmbL9AxEpALqvp3GdQgILF0JBXtM5evk",
           },
         };
-        const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
-          busqueda
-        )}&include_adult=false&language=es-ES&page=1`;
+        const url = `https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent( busqueda )}&include_adult=false&language=es-ES&page=1`;
+        // const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+        //   busqueda
+        // )}&include_adult=false&language=es-ES&page=1`;
         fetch(url, options)
           .then((response) => response.json())
           .then((response) => {
             const results = response.results;
-            const movie = results[0];
-            const idPeli = movie ? movie.id : null;
+            const serieResultado = results[0];
+            const idSerie = serieResultado ? serieResultado.id : null;
 
             // Crea una nueva etiqueta de tarjeta (card)
-            const pelicula = document.createElement("div");
-            pelicula.classList.add("card");
+            const serie = document.createElement("div");
+            serie.classList.add("card");
 
             // Crea el contenido interno de la tarjeta
             const enlace = document.createElement("a");
             const titulo = document.createElement("h3");
             const id_coleccion = "clave=" + encodeURIComponent(elemento.clave);
-            const id_peliculaApi = "id=" + encodeURIComponent(idPeli);
+            const id_serieApi = "id=" + encodeURIComponent(idSerie);
             enlace.href =
-              elemento.enlace + "?" + id_coleccion + "&" + id_peliculaApi;
+              elemento.enlace + "?" + id_coleccion + "&" + id_serieApi;
             titulo.textContent = elemento.titulo;
 
             const imagen = document.createElement("img");
@@ -44,11 +45,11 @@ if (contenedorPeliculas) {
 
             // Agrega el contenido interno a la tarjeta
             enlace.appendChild(titulo);
-            pelicula.appendChild(imagen);
-            pelicula.appendChild(enlace);
+            serie.appendChild(imagen);
+            serie.appendChild(enlace);
 
             // Agrega la tarjeta al contenedor principal
-            contenedorPeliculas.appendChild(pelicula);
+            contenedorSeries.appendChild(serie);
           })
           .catch((error) => {
             console.error("Error al procesar la respuesta de la API:", error);
