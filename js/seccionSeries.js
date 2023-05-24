116bc6fc0e2512078064459d5f332d00a4bdb2a6
ja-calderon <105570149+ja-calderon@users.noreply.github.com>
2023-05-24 18:15:32 -0300

    Se aplicaron cambios en la sección Series

--- js/seccionSeries.js
+++ js/seccionSeries.js
@@ -28,70 +28,65 @@ function filtrarTarjetas(valor) {
 }
 
 function cargarTodasLasTarjetas() {
-  const contenedorPeliculas = document.getElementById("contenedorPeliculas");
+  const contenedorSeries = document.getElementById("contenedorSeries");
 
-  if (contenedorPeliculas) {
-    fetch("datosPelicula.json")
-      .then((response) => response.json())
-      .then((data) => {
-        contenedorPeliculas.innerHTML = "";
-        data.forEach((elemento) => {
-          const busqueda = elemento.busqueda;
-          const options = {
-            method: "GET",
-            headers: {
-              accept: "application/json",
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTUxYzUwOWRlZTBmMjhiYmNkZWI2NjU0NWQ3YWNlMCIsInN1YiI6IjY0Njk3MDhhMzNhMzc2MDE3NWQyZmFmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5KqAXH6kuCBfmbL9AxEpALqvp3GdQgILF0JBXtM5evk",
-            },
-          };
-          const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
-            busqueda
-          )}&include_adult=false&language=es-ES&page=1`;
-          fetch(url, options)
-            .then((response) => response.json())
-            .then((response) => {
-              const results = response.results;
-              const movie = results[0];
-              const idPeli = movie ? movie.id : null;
+if (contenedorSeries) {
+  fetch("datosSeries.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((elemento) => {
+        const busqueda = elemento.busqueda;
+        const options = {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTUxYzUwOWRlZTBmMjhiYmNkZWI2NjU0NWQ3YWNlMCIsInN1YiI6IjY0Njk3MDhhMzNhMzc2MDE3NWQyZmFmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5KqAXH6kuCBfmbL9AxEpALqvp3GdQgILF0JBXtM5evk",
+          },
+        };
+        const url = `https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent( busqueda )}&include_adult=false&language=es-ES&page=1`;
 
-              // Crea una nueva etiqueta de tarjeta (card)
-              const pelicula = document.createElement("div");
-              pelicula.classList.add("card");
+        fetch(url, options)
+          .then((response) => response.json())
+          .then((response) => {
+            const results = response.results;
+            const serieResultado = results[0];
+            const idSerie = serieResultado ? serieResultado.id : null;
 
-              // Crea el contenido interno de la tarjeta
-              const enlace = document.createElement("a");
-              const titulo = document.createElement("h3");
-              const id_coleccion =
-                "clave=" + encodeURIComponent(elemento.clave);
-              const id_peliculaApi = "id=" + encodeURIComponent(idPeli);
-              enlace.href =
-                elemento.enlace + "?" + id_coleccion + "&" + id_peliculaApi;
-              titulo.textContent = elemento.titulo;
+            // Crea una nueva etiqueta de tarjeta (card)
+            const serie = document.createElement("div");
+            serie.classList.add("card");
 
-              const imagen = document.createElement("img");
-              imagen.src = elemento.imagen;
-              imagen.alt = "Imagen de la tarjeta";
+            // Crea el contenido interno de la tarjeta
+            const enlace = document.createElement("a");
+            const titulo = document.createElement("h3");
+            const id_coleccion = "clave=" + encodeURIComponent(elemento.clave);
+            const id_serieApi = "id=" + encodeURIComponent(idSerie);
+            enlace.href =
+              elemento.enlace + "?" + id_coleccion + "&" + id_serieApi;
+            titulo.textContent = elemento.titulo;
 
-              // Agrega el contenido interno a la tarjeta
-              enlace.appendChild(titulo);
-              pelicula.appendChild(imagen);
-              pelicula.appendChild(enlace);
+            const imagen = document.createElement("img");
+            imagen.src = elemento.imagen;
+            imagen.alt = "Imagen de la tarjeta";
 
-              // Agrega la tarjeta al contenedor principal
-              contenedorPeliculas.appendChild(pelicula);
-            })
-            .catch((error) => {
-              console.error("Error al procesar la respuesta de la API:", error);
-            });
-        });
-      })
-      .catch((error) => {
-        console.error("Error al cargar los datos:", error);
+            // Agrega el contenido interno a la tarjeta
+            enlace.appendChild(titulo);
+            serie.appendChild(imagen);
+            serie.appendChild(enlace);
+
+            // Agrega la tarjeta al contenedor principal
+            contenedorSeries.appendChild(serie);
+          })
+          .catch((error) => {
+            console.error("Error al procesar la respuesta de la API:", error);
+          });
       });
-  } else {
-    console.error(
-      "El elemento contenedorPeliculas no fue encontrado en el DOM."
-    );
-  }
+    })
+    .catch((error) => {
+      console.error("Error al cargar los datos:", error);
+    });
+} else {
+  console.error("El elemento contenedorPeliculas no fue encontrado en el DOM.");
+}
 }
